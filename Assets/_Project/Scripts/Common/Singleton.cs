2e2b5ecd863bf34d0d1b620b09b5c20231acc07c
options15 <options15@mail.ru>
2023-03-22 00:15:51 +0400

    add squadconroller

---
+++ Assets/_Project/Scripts/Common/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                    Debug.LogError($"Instance of {typeof(T)} not found");
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+            _instance = (T)this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+}
